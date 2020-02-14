96f5907555a3b2937d2bede5a38d3061e31bc18b
Javori Smart <jsmart1993@hotmail.com>
2020-02-14 16:12:18 -0500

    finished  pages functions

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const store = {
       correctAnswer: 'George Soros'
     }
   ],
-  questionNumber: 1,
-  score: 1,
+  questionNumber: 0,
+  score: 0,
   quizStarted: false,
 };
 
@@ -118,6 +118,8 @@ function generateLandingPageString() {
 
 // question page
 function generateQuestionPageString() {
+  let questionArray = store.questions[store.questionNumber - 1].answers.map(e => e);
+  
   let questionString = store.questions[store.questionNumber - 1].question;
   let questionPageString = `<section class="question-screen" id="question-one">
   <header>
@@ -127,15 +129,15 @@ function generateQuestionPageString() {
   <main>
     <h1>${questionString}</h1>
     <form>
-      <input type="radio" id="answer-one" name="answer" value="answer">
-      <label for="answer">Satoshi Nakamoto</label><br>
-      <input type="radio" id="answer-two" name="answer" value="female">
-      <label for="answer">Benjamin Graham</label><br>
-      <input type="radio" id="answer-three" name="answer" value="other">
-      <label for="answer">Bernie Madoff</label><br>
-      <input type="radio" id="answer-four" name="answer" value="other">
-      <label for="answer">David Einhorn</label><br>
-      <button type="submit">Submit</button>
+      <input type="radio" id="choice-one" name="answer">
+      <label for="answer">${questionArray[0]}</label><br>
+      <input type="radio" id="choice-two" name="answer">
+      <label for="answer">${questionArray[1]}</label><br>
+      <input type="radio" id="choice-three" name="answer">
+      <label for="answer">${questionArray[2]}</label><br>
+      <input type="radio" id="choice-four" name="answer">
+      <label for="answer">${questionArray[3]}</label><br>
+      <button type="submit" id='submit-button'>Submit</button>
     </form>
 
   </main>
@@ -145,14 +147,73 @@ function generateQuestionPageString() {
 }
 
 // feedback page
+function generateFeedbackPageCorrectString() {
+  let feedbackPageString =  `<section class="feedback-screen-correct">
+    <header>
+      <h1>Correct!</h1>
+    </header>
+    <main>
+      <p>Your score is now: ${store.score}</p>
+      <button type='submit'>Continue</button>
+      <p>Press continue to move on to question ${store.questionNumber + 1}</p>;
+    </main>
+  </section>`;
+  
+  return feedbackPageString;
+}
 
+function generateFeedbackPageIncorrectString() {
+  let correctAnswerString = store.questions[store.questionNumber - 1].correctAnswer;
+  let feedbackPageString =  `<section class="feedback-screen-incorrect">
+  <header>
+    <h1>Sorry, that was incorrect.</h1>
+  </header>
+  <main>
+    <p>The correct answer is: ${correctAnswerString}</p>
+    <p>Your score is now: ${store.score}</p>
+    <button type='submit'>Continue</button>
+    <p>Press continue to move on to question ${store.questionNumber + 1}</p>
+  </main>
+</section>`;
+
+  return feedbackPageString;
+}
 // summary page
+
+function generateSummaryPageString() {
+  let summaryPageString =  `<section class="Summary Screen">
+  <header>
+    <h1>Quiz Over!</h1>
+  </header>
+  <main>
+    <p>You got ${store.score}/5 Questions Correct</p>
+    <button type='reset'>Retake Quiz</button>
+    <p>Press the retake quiz button to take the quiz again</p>
+  </main>
+</section>`; 
+}
 
 /** EVENT HANDLER FUNCTIONS */
 
 // landing page start quiz submit event
+function landingPageSubmitEventHandler() {
+ ${'form'}.on('submit', '.start-button', event => {
+    event.preventDefault();
+    store.quizStarted = true;
+    //render();
+ }
+ )
+}
 
 // question page submit answer event
+
+function questionPageSubmitAnswerEventHandler() {
+  $('form').on('submit', '#submit-button', event => {
+    event.preventDefault();
+      if()
+  }
+  )
+ }
 
 // feedack page continue to next question submit event
 
@@ -171,6 +232,8 @@ function reset() {
 function runQuizApp() {
   render();
   console.log(generateQuestionPageString());
+  console.log(generateFeedbackPageCorrectString());
+  console.log(generateFeedbackPageIncorrectString());
   
   
   // call each event handler function
